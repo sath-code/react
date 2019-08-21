43c42fe5a5fef95f52099effefde2a92c55ad404
sath-code <sathish.dinakar@gmail.com>
2019-08-21 14:38:35 +0530

    first commit

---
+++ new-project/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+/*import Welcome from './component/Welcome';*/
+
+ReactDOM.render(
+  <App />,
+  /*<Welcome />,*/
+  document.getElementById('root')
+);
